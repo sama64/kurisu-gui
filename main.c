da3758867ac5c1e98188cdc37ca81ca312e6daaf
yeriko <lisandrotobias@gmail.com>
2024-07-13 23:46:21 -0300

    x bug

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <GLFW/glfw3.h>
 #include <leif/leif.h>
 
+#include <stdio.h>
+
 
 #include "config.h"
 
@@ -27,11 +29,13 @@ typedef struct {
   entry_priority priority;
 } todo_entry;
 
-static LfFont titlefont;
+static LfFont titlefont, smallfont;
 static todo_filter current_filter;
 
 static todo_entry* entries[1024];
 static uint32_t numEntries = 0;
+
+static LfTexture removeTexture;
 
 
 static void rendertopbar() {
@@ -103,7 +107,106 @@ static void renderfilters() {
     lf_pop_style_props();  // Restaurar el estilo después de terminar los filtros
 }
 
-int main(int argc, char **argv) {
+static void renderlist()
+{
+
+    lf_div_begin(((vec2s){lf_get_ptr_x(), lf_get_ptr_y()}), ((vec2s){WIN_INIT_W - lf_get_ptr_x() - GLOBAL_MARGIN, WIN_INIT_H - lf_get_ptr_y() - GLOBAL_MARGIN}), true);
+
+    uint32_t renderedcount = 0;
+
+    for (uint32_t i = 0; i < numEntries; i++)
+    {
+        todo_entry* entry = entries[i];
+        if(current_filter == FILTER_LOW && entry->priority != PRIORITY_LOW) continue;
+        if(current_filter == FILTER_MEDIUM && entry->priority != PRIORITY_MEDIUM) continue;
+        if(current_filter == FILTER_HIGH && entry->priority != PRIORITY_HIGH) continue;
+        if(current_filter == FILTER_COMPLETED && !entry->completed) continue;
+        if(current_filter == FILTER_IN_PROGRESS && entry->completed) continue;
+        float priority_size = 15.0f;
+        float ptry_before = lf_get_ptr_y();
+        lf_set_ptr_y_absolute(lf_get_ptr_y() + 5.0f);
+        lf_set_ptr_x_absolute(lf_get_ptr_x() + 5.0f);
+        switch (entry->priority)
+        {
+            case PRIORITY_LOW:{
+                lf_rect(priority_size, priority_size, (LfColor){75, 175, 80, 255}, 4.0f);
+                break;
+            }
+            case PRIORITY_MEDIUM:{
+                lf_rect(priority_size, priority_size, (LfColor){255, 235, 59, 255}, 4.0f);
+                break;
+            }
+            case PRIORITY_HIGH:{
+                lf_rect(priority_size, priority_size, (LfColor){244, 67, 54, 255}, 4.0f);
+                break;
+            }
+        }
+        lf_set_ptr_y_absolute(ptry_before);
+
+        {
+            LfUIElementProps props = lf_get_theme().button_props;
+            props.color = LF_NO_COLOR;
+            props.border_width = 0.0f;
+            props.padding = 0.0f;
+            props.margin_top = 0.0f;
+            props.margin_left = 10.0f;
+            lf_push_style_props(props);
+            if(lf_image_button(((LfTexture){.id = removeTexture.id, .width = 20, .height = 20})) == LF_CLICKED)
+            {
+                for(uint32_t j = i; j < numEntries - 1; j++)
+                {
+                    entries[j] = entries[j + 1];
+                }
+                numEntries--;
+            }
+            lf_pop_style_props();
+        }
+
+        {
+            LfUIElementProps props = lf_get_theme().checkbox_props;
+            props.border_width = 1.0f;
+            props.corner_radius = 0.0f;
+            props.margin_top = 0;
+            props.padding = 5.0f;
+            props.margin_left = 10.0f;
+            props.color = lf_color_from_zto((vec4s){0.05f, 0.05f, 0.05f, 1.0f});
+            lf_push_style_props(props);
+            if(lf_checkbox("", &entry->completed, LF_NO_COLOR, ((LfColor){65, 167, 204, 255})) == LF_CLICKED){
+
+            }
+            lf_pop_style_props();
+        }
+        lf_push_font(&smallfont);
+        LfUIElementProps props = lf_get_theme().text_props;
+        props.margin_top = 0.0f;
+        props.margin_left = 5.0f;
+        lf_push_style_props(props);
+
+        float descprt_x = lf_get_ptr_x();
+        lf_text(entry->desc);
+
+        lf_set_ptr_x_absolute(descprt_x);
+        lf_set_ptr_y_absolute(lf_get_ptr_y() + smallfont.font_size);
+        props.text_color = (LfColor){150, 150, 150, 255};
+        lf_push_style_props(props);
+        lf_text(entry->date);
+        lf_pop_style_props();
+        lf_pop_font();
+
+        lf_next_line();
+        
+        renderedcount++;
+    } 
+
+    if(!renderedcount){
+        lf_text("There is no task here.");
+    }
+    lf_div_end();
+         
+}
+
+int main(int argc, char **argv) 
+{
     glfwInit();
     GLFWwindow* window = glfwCreateWindow(WIN_INIT_W, WIN_INIT_H, "Todo", NULL, NULL);
 
@@ -116,13 +219,19 @@ int main(int argc, char **argv) {
     lf_set_theme(theme);
 
     titlefont = lf_load_font("./fonts/inter-bold.ttf", 40);
+    smallfont = lf_load_font("./fonts/inter.ttf", 20);
+
+    removeTexture = lf_load_texture("./icons/remove.png", true, LF_TEX_FILTER_LINEAR);
+
+    for(uint32_t i = 0; i< 5; i++){
 
     todo_entry* entry = (todo_entry*)malloc(sizeof(*entry));
-    entry->priority = PRIORITY_LOW;
+    entry->priority = PRIORITY_MEDIUM;
     entry->completed = false;
     entry->date = "nothing";
     entry->desc = "Comprarle un pancho a pancho";
     entries[numEntries++] = entry;
+    }
 
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
@@ -136,39 +245,10 @@ int main(int argc, char **argv) {
         lf_next_line();
         renderfilters();
         lf_next_line();
+        renderlist();
 
-        {
-            lf_div_begin(((vec2s){lf_get_ptr_x(), lf_get_ptr_y()}), ((vec2s){WIN_INIT_W - lf_get_ptr_x() - GLOBAL_MARGIN, WIN_INIT_H - lf_get_ptr_y() - GLOBAL_MARGIN}), true);
+        
 
-            for (uint32_t i = 0; i < numEntries; i++)
-            {
-                todo_entry* entry = entries[i];
-                float priority_size = 15.0f;
-                float ptry_before = lf_get_ptr_y();
-                lf_set_ptr_y_absolute(lf_get_ptr_y() + 5.0f);
-                lf_set_ptr_x_absolute(lf_get_ptr_x() + 5.0f);
-                switch (entry->priority)
-                {
-                    case PRIORITY_LOW:{
-                        lf_rect(priority_size, priority_size, (LfColor){75, 175, 80, 255}, 4.0f);
-                        break;
-                    }
-                    case PRIORITY_MEDIUM:{
-                        lf_rect(priority_size, priority_size, (LfColor){255, 235, 59, 255}, 4.0f);
-                        break;
-                    }
-                    case PRIORITY_HIGH:{
-                        lf_rect(priority_size, priority_size, (LfColor){244, 67, 54, 255}, 4.0f);
-                     break;
-                    }
-                }
-                lf_set_ptr_y_absolute(ptry_before);
-                LfUIElementProps props = lf_get_theme().text_props;
-                props.margin_top = 10.0f;
-                lf_text(entry->desc);
-                lf_next_line();
-            }    
-        }   
 
         lf_div_end();
         lf_end();
